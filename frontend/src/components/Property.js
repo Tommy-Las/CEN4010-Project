bfa6735088c2548d40bedfc2f2cf10c38edce0d6
Pedilus <mspedilus@yahoo.com>
2022-07-13 22:06:46 -0400

    Added get and post methods

--- frontend/src/components/Property.js
+++ frontend/src/components/Property.js
@@ -1,4 +1,39 @@
+import React, {useState} from "react"
+import axios from "axios"
+
 function Property(){ 
-    return <div>Single Property Information</div>}
+    const [location, setLocation] = useState(""); //Stores submitted location 
+    const [homeInfo, setHomeInfo] = useState({}); //Stores home information
+  
+  
+    //Update location state 
+    function handleChange(event) {
+      const {value} = event.target;
+      setLocation(value);
+    }
+  
+  
+    //Does get request to find home with matching location
+    function search(event) {
+      event.preventDefault(); //Stops the page from refreshing after pressing the submit button
+  
+      //Performs get method to find home(s) with matching location
+      axios.get("http://localhost:8080", {params: location})
+           .then((res) => setHomeInfo(res.data)) //If request was successful store data found in homeInfo                   
+           .catch(err => {alert("No homes found"); //If request was unsuccessful display error message
+                          console.log(err)});  
+      }
+  
+      
+    return (
+        <form onSubmit={search}>
+            <h3>Single Property Information</h3>
+            <label htmlFor="location">Location: </label>
+            <input type="text" id="location" name="location" value={location} placeholder="Location" onChange={handleChange} />
+            <br />
+            <button>Search</button>
+        </form>
+    )
+}
 
 export default Property
