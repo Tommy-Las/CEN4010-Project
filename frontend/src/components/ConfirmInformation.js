6680ea22d763258deddc3d87a9285b91371932d1
TommyLas <tlas2019@fau.edu>
2022-07-28 00:57:06 -0400

    confirmation page css done

--- frontend/src/components/ConfirmInformation.js
+++ frontend/src/components/ConfirmInformation.js
@@ -18,13 +18,14 @@ export default function ConfirmInformation() {
 }
 
   return (
-    <form onSubmit={handleSubmit} id='form-confirmation'> 
+    <form onSubmit={handleSubmit} id='form-confirmation'>
+      <h1>Enter your information</h1>
       <label >Full Name:</label>
       <input  placeholder='first name' className="name-input" value={first_name} onChange={(e) => setFirstName(e.target.value)}></input>
       <input  placeholder='last name' className="name-input" value={last_name} onChange={(e) => setLastName(e.target.value)}></input>
       <label >Address:</label>
-      <input  id="addressInput" value={address} onChange={(e) => setAddress(e.target.value)}></input>
-      <button id="loginButton">Submit</button>
+      <input  id="address-input" value={address} onChange={(e) => setAddress(e.target.value)}></input>
+      <button className="loginButton">Submit</button>
     </form>
   )
 }
